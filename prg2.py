5b7a84e99d5f5931aded3f75c055d9db53553c6e
Coderrajofficial <raj.sukdev.mahajan@gmail.com>
2025-08-15 17:18:37 +0530

    Create prg2.py

---
+++ prg2.py
@@ -0,0 +1,6 @@
+r=eval(input('enter the radius of cylinder : '))
+h=eval(input('enter the height of cylinder : '))
+sa=2*pi*r*r
+v=pi*r*r*h
+print('surface area of cylinder is =',sa)
+print('volume of cylinder is =',v)
